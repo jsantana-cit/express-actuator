68da0655a8363fb530d362850f4b8d0e141169ee
Jefferson Carvalho Santana <jsantana@lnxcit021777.cit>
2019-02-26 15:37:42 -0300

    First implementation

--- test/unit/lib/infoRouteSpec.js
+++ test/unit/lib/infoRouteSpec.js
@@ -21,51 +21,66 @@ describe('info route', function () {
         };
     });
 
-    it('should send status code 200', function () {
+    it('should send status code 200', function (done) {
         infoRoute(null, response);
 
-        expect(response.status).to.have.been.calledOnce;
-        expect(response.status).to.have.been.calledWithExactly(200);
+        setTimeout(function(){
+            expect(response.status).to.have.been.calledOnce;
+            expect(response.status).to.have.been.calledWithExactly(200);
+            done();
+        }, 1);
     });
 
-    it('should send a payload with all keys', function () {
+    it('should send a payload with all keys', function (done) {
         infoRoute(null, response);
 
-        expect(response.json).to.have.been.calledOnce;
-        expect(response.json).to.have.property('name');
+        setTimeout(function(){
+            expect(response.json).to.have.been.calledOnce;
+            expect(response.json).to.have.property('name');
+            done();
+        }, 1);
     });
 
-    it('should send a payload when process.env.npm_package_name does not exists', function () {
+    it('should send a payload when process.env.npm_package_name does not exists', function (done) {
         var infoRouteRewire = rewire("../../../lib/infoRoute");
         infoRouteRewire.__set__("envPackageName", null);
 
         infoRouteRewire(null, response);
 
-        expect(response.json).to.have.been.calledOnce;
-        expect(response.json).to.have.property('name');
+        setTimeout(function(){
+            expect(response.json).to.have.been.calledOnce;
+            expect(response.json).to.have.property('name');
+            done();
+        }, 1);
     });
 
-    it('should send a payload when process.env.npm_package_name does not exists and git.properties exists', function () {
+    it('should send a payload when process.env.npm_package_name does not exists and git.properties exists', function (done) {
         var infoRouteRewire = rewire("../../../lib/infoRoute");
         infoRouteRewire.__set__("envPackageName", null);
 
         mock({
           'git.properties': "git.branch=master\ngit.commit.id.abbrev=1324324\ngit.commit.time=2016-11-18T13:16:39.000Z",
-          'package.json': "\{\"name\":\"eor\"\}"
+          'package.json': "{\"name\":\"eor\"}"
         });
 
         infoRouteRewire(null, response);
 
-        expect(response.json).to.have.been.calledOnce;
-        expect(response.json).to.have.property('name');
+        setTimeout(function(){
+            expect(response.json).to.have.been.calledOnce;
+            expect(response.json).to.have.property('name');
+            done();
+        }, 1);
 
         mock.restore();
     });
 
-    it('should flush the response sending', function () {
+    it('should flush the response sending', function (done) {
         infoRoute(null, response);
 
-        expect(response.end).to.have.been.calledOnce;
-        expect(response.end).to.have.been.calledWithExactly();
+        setTimeout(function(){
+            expect(response.end).to.have.been.calledOnce;
+            expect(response.end).to.have.been.calledWithExactly();
+            done();
+        }, 1);
     });
 });
